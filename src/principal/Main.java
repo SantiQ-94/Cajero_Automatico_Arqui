1c7b794fa80e7c83a1aefc052137c1ac1fef4d3b
Spitpat <san.quit@hotmail.com>
2015-10-19 19:59:14 -0400

    nada en especial

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -13,23 +13,7 @@ import GUI.VistaRetiroRapido;
  * @author Quiroga
  */
 public class Main {
-    public static void main(String []args) {
-        //LoginNroDeCuenta login = new LoginNroDeCuenta();
-        //LoginPin panelPin = new LoginPin();
-        //VistaPrincipal vp = new VistaPrincipal();
-        //VistaRetiroRapido vrr = new VistaRetiroRapido();
-        //VistaRetiro vr = new VistaRetiro();
-        //VistaDeposito vd = new VistaDeposito();
-        //VistaCambioDePin vcdp = new VistaCambioDePin();
-        
-        //login.setVisible(true);
-        //panelPin.setVisible(true);
-        //vp.setVisible(true);
-//        vrr.setVisible(true);
-//        vr.setVisible(true);
-        //vd.setVisible(true);
-       // vcdp.setVisible(true);
-        
+    public static void main(String []args) {    
         Cajero cajero = new Cajero();
     }
 }
